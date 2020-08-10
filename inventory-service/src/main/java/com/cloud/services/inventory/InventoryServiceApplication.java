77a9c1658613bf4e817f9ef982c1df921a22ee30
KevinMarioGerard <kevinmariogerard@gmail.com>
2020-08-10 05:48:05 +0530

    Dockerizing Inventory service
    1. Adding DockerFile for dockerizing inventory service
    2. Adding scripts to build, start and destroy services

--- inventory-service/src/main/java/com/cloud/services/inventory/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/cloud/services/inventory/InventoryServiceApplication.java
@@ -2,12 +2,18 @@ package com.cloud.services.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InventoryServiceApplication {
+public class InventoryServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(InventoryServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
-
 }
