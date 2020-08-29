216308d46cbcb0b30ac9168191eb41c7e9463bb3
Sanjay Suresh <geekysanjayofficial@gmail.com>
2020-08-30 01:42:06 +0530

    1st commit

--- inventory-service/src/main/java/com/cloud/services/inventory/rest/HelloController.java
+++ inventory-service/src/main/java/com/cloud/services/inventory/rest/HelloController.java
@@ -3,12 +3,12 @@ package com.cloud.services.inventory.rest;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@RestController // Rest Controller
 public class HelloController {
 
 	@RequestMapping("/helloworld")
 	public String index() {
-		return "Hello World!";
+		return "Hello World!!!!!";
 	}
 
 }
